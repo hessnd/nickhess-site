ae18c8937ff542be0669f1056ec3051cfe8d80a9
hessnd <n.hess@bigspaceship.com>
2017-08-29 21:53:20 -0400

    added items to head

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Head from 'next/head';
 
 import stylesheet from 'styles/index.scss';
 
 export default () => (
   <div>
+    <Head>
+      <title>Nick Hess</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+    </Head>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }}/>
     <h1>My Site</h1>
   </div>
