747524580a5b57e377c088ebe060418afc394b5f
hessnd <n.hess@bigspaceship.com>
2017-08-29 22:55:08 -0400

    some styling

--- components/Bio.js
+++ components/Bio.js
@@ -1,7 +1,7 @@
 export default () => (
   <div className="bio">
     <h2>Nick Hess</h2>
-    <p>Brooklyn based developer working at <a href="https://www.bigspaceship.com" target="_blank">Big Spaceship</a></p>
+    <p>Brooklyn based developer working at <a href="https://www.bigspaceship.com" target="_blank">Big Spaceship</a>.</p>
     <img src="/static/upside-down-smiley.png" alt="upside down smiley"/>
     <div className="links">
       <a href="https://twitter.com/nickdhess" target="_blank">Twitter</a>
