e2bdaadf6786ee7b1770dd7e3802730f100a9b11
hessnd <n.hess@bigspaceship.com>
2017-08-29 22:07:34 -0400

    added static files and favicon

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,8 @@ export default () => (
   <div>
     <Head>
       <title>Nick Hess</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      <meta name='viewport' content='initial-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=no'/>
+      <link rel='icon' href='/static/favicon.icon'/>
     </Head>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }}/>
     <h1>My Site</h1>
